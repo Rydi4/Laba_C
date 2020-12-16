0f9cbf7a8a8d09ecae62f0098ecfbccbed0069af
Rydi4 <72516588+Rydi4@users.noreply.github.com>
2020-12-17 01:34:18 +0200

    Laba 6

---
+++ Laba6/main.c
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+int func(char text[100], int word ) 
+{
+	for (int i = 0; i < strlen(text); i++) 
+	{
+		if (text[i-1] != ' ' && text[i] == ' ' && text[i+1] != ' ') 
+		{
+			word++;
+		}
+	}
+	int i = strlen(text);
+	if (text[0] != ' ' && text[i - 1] != ' ')
+	{
+		word++;
+	}
+	if (text[0] == ' ' && text[i - 1] == ' ')
+	{
+		word--;
+	}
+	printf("Количество слов: %d", word);
+	return 0;
+}
+int main()
+{ 
+	setlocale(LC_ALL, "Rus");
+	char text[100];
+	   printf("Текст: ");
+	   gets_s (text);
+	   int word = 0;
+	   func(text, word);
+	return 0;
+}
+
