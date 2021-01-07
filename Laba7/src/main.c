0416ff7f8ea59ae43556e4b1cfbdbc7f65c2b8ff
Rydi4 <72516588+Rydi4@users.noreply.github.com>
2021-01-07 19:12:53 +0200

    Laba7

--- Laba7/src/main.c
+++ Laba7/src/main.c
@@ -1,6 +1,23 @@
 ﻿#include <locale.h>
 #include <iostream>
-#include <stdarg.h>
+#include <stdarg.h>м
+
+/**
+* @file main.c виконати завдання 4
+*
+* порівняти пари чисел у котрих перше число менше наступного
+*/
+
+/**
+* виконати завдання за допомогою функції з вариативною кількістю аргументів
+* @param answer - відповідь;
+* @param number1 - перше число;
+* @param numer2 - друге число;
+*/
+
+/**
+ Функція 'comparison_of_numbers'. Порівнює пари чисел.
+*/
 
 int comparison_of_numbers(int num, ...)
 {
@@ -33,6 +50,9 @@ int comparison_of_numbers(int num, ...)
 	printf("%d\n", answer);
 	return 0;
 }
+/**
+ Функція main: Передає значення аргуменів функції 'comparison_of_numbers'.
+*/
 int main()
 {
 	comparison_of_numbers(3, 29, 48, 36); 
